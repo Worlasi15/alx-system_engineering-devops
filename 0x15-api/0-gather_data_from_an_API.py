b85d6bf4d0b2d43b09fe78333f39c3942d29876f
Worlasi15 <efyawalasi@gmail.com>
2023-11-29 15:27:22 -0800

    print out employee's TODO LIST

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Python script that returns information
+about his/her TODO list progress.
 """
 
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
+    employee_id = argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
 
-    # Employee ID provided as a command-line argument
-    employee_id = sys.argv[1]
+    try:
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+        todo_response = requests.get(todo_url)
+        todo_data = todo_response.json()
 
-    # API endpoint URL for the employee's TODO list
-    url = 'https://jsonplaceholder.typicode.com/users
-    /{}/todos'.format(employee_id)
+        total_tasks = len(todo_data)
+        completed_tasks = [task for task in todo_data if task['completed']]
+        num_completed_tasks = len(completed_tasks)
 
-    # Fetching data from the API
-    response = requests.get(url)
-    todos = response.json()
+        print("Employee {} is done with tasks({}/{}):".format(
+            user_data['name'], num_completed_tasks, total_tasks
+        ))
 
-    # Getting employee information from the API
-    employee_info_url = 'https://jsonplaceholder.typicode.com
-    /users/{}'.format(employee_id)
-    employee_info = requests.get(employee_info_url).json()
-    employee_name = employee_info.get('name')
+        for task in completed_tasks:
+            print("\t {}".format(task['title']))
 
-    # Counting the number of completed tasks
-    completed_tasks = [task for task in todos if task['completed']]
-    num_completed_tasks = len(completed_tasks)
-    total_tasks = len(todos)
-
-    # Displaying employee TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format
-            (employee_name, num_completed_tasks, total_tasks))
-
-    # Displaying titles of completed tasks
-    for task in completed_tasks:
-        print("\t {}".format(task['title']))
+    except requests.RequestException as e:
+        print("Error: {}".format(e))
